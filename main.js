ffdc4bc0062c96d3896b39272d4b4f8d79c6ca64
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2023-10-16 20:28:37 +0400

    123

---
+++ main.js
@@ -0,0 +1,7 @@
+var Name, Surname, Age;
+Name = prompt("Please enter your name:");
+Surname = prompt("Please enter your surname:");
+Age = prompt("Please enter your age:");
+console.log("Name: " + Name);
+console.log("Surname: " + Surname);
+console.log("Age: " + Age);
