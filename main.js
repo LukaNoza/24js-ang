6a3dd3899732f723b2f6ae682a9a8843c3c22405
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2023-10-16 20:36:47 +0400

    123finish

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-var Name, Surname, Age;
-Name = prompt("Please enter your name:");
-Surname = prompt("Please enter your surname:");
-Age = prompt("Please enter your age:");
-console.log("Name: " + Name);
-console.log("Surname: " + Surname);
-console.log("Age: " + Age);
+// 1
+// var Name, Surname, Age;
+// Name = prompt("Please enter your name:");
+// Surname = prompt("Please enter your surname:");
+// Age = prompt("Please enter your age:");
+// console.log("Name: " + Name);
+// console.log("Surname: " + Surname);
+// console.log("Age: " + Age);
+
+
+// 2
+// var firstName = "Luka";
+// var lastName = "Nozadze";
+// var email = "lukakalu@gmail.com";
+// var age = 16;
+// var gender = "Male";
+
+// var combinedInfo = "Name: " + firstName + " " + lastName + "\n" +
+//                   "Email: " + email + "\n" +
+//                   "Age: " + age + "\n" +
+//                   "Gender: " + gender;
+
+// var paragraph = document.createElement("p");
+// paragraph.textContent = combinedInfo;
+// document.body.appendChild(paragraph);
+
+
+// 3
+// var fullName = prompt("Please enter your full name:");
+// var h1Element = document.createElement("h1");
+// h1Element.textContent = fullName;
+// document.body.appendChild(h1Element);
